7ace251acff771756a3251809518aef49e889216
cn-zheshi <1760864645@qq.com>
2021-05-07 14:51:14 +0800

    解析命令行输入

--- src/main/java/nju/yajhttp/Client.java
+++ src/main/java/nju/yajhttp/Client.java
@@ -1,7 +1,15 @@
 package nju.yajhttp;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.File;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 import lombok.Cleanup;
 import lombok.SneakyThrows;
@@ -11,23 +19,106 @@ import nju.yajhttp.message.URI;
 import nju.yajhttp.message.Version;
 
 public class Client {
+
+    Map<String, String> header = new HashMap<String, String>();
+    ArrayList<String> body = new ArrayList<String>();// 需要转成byte
+    FileOutputStream fos;
+    Method method = Method.GET;
+    URI uri;
+    boolean ish = false;
+
+    public void parse(String[] args) throws FileNotFoundException {
+        // TODO:headers的name需要化成标准格式;
+        for (int i = 0; i < args.length; ++i) {
+            if (args[i].equals("-h") || args[i].equals("--help")) {
+                ish = true;
+                System.out.println("helpString");// helpString的值需要更换
+                return;
+            }
+        }
+        for (int i = 0; i < args.length; ++i) {
+            switch (args[i]) {
+                case "-u":
+                case "--user":
+                    String tempstr = "Basic "
+                            + Base64.getEncoder().encodeToString(args[++i].getBytes(StandardCharsets.US_ASCII));
+                    header.put("authorization", tempstr);
+                    break;
+                case "-o":
+                    String pathname = args[++i];
+                    // TODO: pathname
+                    fos = new FileOutputStream(new File(pathname));
+                    break;
+                case "-d":
+                case "--data":
+                    method = Method.POST;
+                    body.add(args[++i]);
+                    header.put("content-type", "application/x-www-form-urlencoded");
+                    break;
+                case "-H":
+                case "--header":
+                    String[] arg = args[++i].split(":");
+                    header.put(arg[0].toLowerCase(), arg[1]);
+                    break;
+                default:
+                    uri = new URI(args[i]);
+                    break;
+            }
+        }
+    }
+
     @SneakyThrows
     public static void main(String[] args) {
-        var uri = new URI(args[0]);
-        var port = 0;
-        switch (uri.scheme()) {
-        case "http":
-            port = 80;
-            break;
-        default:
-            throw new Error();
+        /*
+         * var uri = new URI(args[0]); var port = 0; switch (uri.scheme()) { case
+         * "http": port = 80; break; default: throw new Error(); } var request = new
+         * Request().method(Method.GET).version(Version.HTTP1_0).uri(uri).header("Host",
+         * uri.host());
+         * 
+         * @Cleanup var socket = new Socket(uri.host(), port); var os =
+         * socket.getOutputStream(); request.write(os); var is =
+         * socket.getInputStream(); System.out.println(new String(is.readAllBytes(),
+         * StandardCharsets.UTF_8));
+         */
+        Client client = new Client();
+        try {
+            client.parse(args);
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found");
+            return;
         }
-        var request = new Request().method(Method.GET).version(Version.HTTP1_0).uri(uri).header("Host", uri.host());
+        if (client.ish) {
+            return;
+        }
+        int port = 0;
+        switch (client.uri.scheme()) {
+            case "http":
+                port = 8000;
+                break;
+            default:
+                throw new Error();
+        }
+        Request request = new Request().method(client.method).version(Version.HTTP1_0).uri(client.uri).header("Host",
+                client.uri.host());
+        if (!client.body.isEmpty()) {
+            String tempStr = String.join("&", client.body);
+            byte[] bytes = new String(tempStr).getBytes();
+            request.body(bytes);
+            client.header.put("content-length", String.valueOf(bytes.length));
+        }
+        for (var i : client.header.keySet()) {
+            request.header(i, client.header.get(i));
+        }
         @Cleanup
-        var socket = new Socket(uri.host(), port);
+        Socket socket = new Socket(client.uri.host(), port);
         var os = socket.getOutputStream();
         request.write(os);
         var is = socket.getInputStream();
-        System.out.println(new String(is.readAllBytes(), StandardCharsets.UTF_8));
+        if (client.fos == null) {
+            System.out.println(new String(is.readAllBytes(), StandardCharsets.UTF_8));
+        }
+        else{
+            client.fos.write(is.readAllBytes());
+        }
     }
 }
